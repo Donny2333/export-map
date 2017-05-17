b9354ed5aed306c26519cfc2235c6975a84e3a5a
Donny <1021001390@qq.com>
2017-05-17 14:09:19 +0800

    add zTree directive

--- js/directives/directive.js
+++ js/directives/directive.js
@@ -39,4 +39,25 @@
             };
         })
 
+        .directive('zTree', ['$parse', function ($parse) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    var treeObj = undefined;
+                    var setting = $parse(attrs.setting)(scope);
+                    var zNodes = $parse(attrs.zNodes)(scope);
+
+                    treeObj = $.fn.zTree.init(element, setting, zNodes);
+
+                    scope.$watch(function () {
+                        return $parse(attrs.zNodes)(scope);
+                    }, function (value) {
+                        if (value) {
+                            treeObj = $.fn.zTree.init(element, setting, value);
+                        }
+                    });
+                }
+            }
+        }])
+
 })(angular);
