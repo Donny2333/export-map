b029266c496a67880a291d42a7f957ccd800319a
Donny <1021001390@qq.com>
2017-05-26 09:22:32 +0800

    set product and development url

--- js/config/config.js
+++ js/config/config.js
@@ -4,16 +4,16 @@
 (function (angular) {
     "use strict";
 
-    var prodURL = 'https://**.***.com/',
-        devURL = 'http://172.30.1.246:9527/',
+    var prodURL = 'https://172.30.1.246:9527/',
+        devURL = 'http://192.168.250.44:9527/',
         Urls = {
             Prod_Cfg: {
                 api: prodURL,
-                img: prodURL
+                img: 'http://172.30.1.246:9528/'
             },
             Dev_Cfg: {
                 api: devURL,
-                img: 'http://172.30.1.246:9528/'
+                img: 'http://192.168.250.44:9528/'
             }
         };
 
